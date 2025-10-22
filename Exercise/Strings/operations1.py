a2f43a1e0fc2783728311749a22d8e7a00a1ce37
YagnaRaval28 <yagnaraval4@gmail.com>
2025-10-22 17:27:26 +0530

    Added Operation's first  file

---
+++ Exercise/Strings/operations1.py
@@ -0,0 +1,21 @@
+str="Python is Fun"
+"""print(str[0])
+print(str[-1])
+print(len(str))
+print(str*3)
+
+#Membership Operation (In)
+print("Python" in str)
+print("Z" in str)
+
+#Not in
+print("Z" not in str)
+print("Java" not in str)
+"""
+#Removing spaces from string- strip()
+print("Python "=="Python")
+s1="Python "
+print(s1.strip()=="Python")
+
+#Replace
+print(str.replace("Python","Java"))
