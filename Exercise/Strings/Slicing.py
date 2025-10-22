d6e4c6944a855040b5eaf7811b937c3f0310284b
YagnaRaval28 <yagnaraval4@gmail.com>
2025-10-22 12:41:47 +0530

    Added README file

--- Exercise/Strings/Slicing.py
+++ Exercise/Strings/Slicing.py
@@ -1,3 +1,5 @@
 str="Hello World"
 #Sytax for Indexing= str[index]
-#Syntax for slicing= str[staring index:ending index]
+#Syntax for slicing= str[staring index:ending index:step(By default it is 1)]
+print(str[2:7:1])
+print(str[2:9:2])
