6601a292949c5597c2fd672ae6be5fa2f1f6ce79
YagnaRaval28 <yagnaraval4@gmail.com>
2025-10-22 13:10:57 +0530

    Added escape_sequence file

---
+++ Exercise/Strings/escape_sequence.py
@@ -0,0 +1,12 @@
+"""
+\n=New Line
+\t=New Tab
+\\=backslash
+\'=Add a single quote inside a single-quoted string
+"""
+print("Hello Everyone?\nHow are you?")
+print("\n")
+print("John 20")
+print("John\t20")
+print("Yes\\No")
+print('This is Python\'s Class')
