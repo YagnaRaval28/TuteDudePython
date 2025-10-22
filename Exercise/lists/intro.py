cbd90e402730dfd96ef03e3360cec04fbfbc977f
YagnaRaval28 <yagnaraval4@gmail.com>
2025-10-22 18:14:04 +0530

    Created lists folder and added intro file

---
+++ Exercise/lists/intro.py
@@ -0,0 +1,13 @@
+name="John"
+age=20
+percent=85.5
+student=[name,age,percent]
+print(student)
+print(type(student))
+
+days_of_week=['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
+print(days_of_week[2])
+print(f"Last day of week is {days_of_week[-1]}")
+
+#Length of list
+print(len(student))
