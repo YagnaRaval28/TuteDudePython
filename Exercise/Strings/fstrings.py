4dc5ef5c350bb953281361f339c91749e81215d2
YagnaRaval28 <yagnaraval4@gmail.com>
2025-10-22 12:57:42 +0530

    Added fstring file

---
+++ Exercise/Strings/fstrings.py
@@ -0,0 +1,16 @@
+name="John"
+language="Python"
+#Without fstring
+#John is studying Python
+print(name,"is studying",language)
+
+#Using Fstrin
+print(f"{name} is studying {language}")
+
+sub1=89
+sub2=82
+sub3=90
+print(f"{name} has scored {sub1+sub2+sub3} in total out of 300")
+
+percentage=(sub1+sub2+sub3)/3
+print(f"{name} got {percentage}%")
