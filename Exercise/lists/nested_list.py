25b8987aa747ced77db7656ddec524f6d2d3cfed
YagnaRaval28 <yagnaraval4@gmail.com>
2025-10-24 18:56:09 +0530

    Added nested list file

---
+++ Exercise/lists/nested_list.py
@@ -0,0 +1,9 @@
+list=[1,[1,2,3],"Hi","Python",5.6,True,None]
+print(f"Length of list is {len(list)}")
+
+print(list[-6][-3])
+
+l2=[[0,1],[1,2],[5,6,[0,1]]]
+print(l2[-1])
+print(l2[-1][-1])
+print(l2[-1][-1][-1])
