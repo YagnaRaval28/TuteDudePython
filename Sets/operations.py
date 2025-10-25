edae36978c70f0c6c5fdd8e46c953371010d9857
YagnaRaval28 <yagnaraval4@gmail.com>
2025-10-25 17:26:27 +0530

    Added operations.py

--- Sets/operations.py
+++ Sets/operations.py
@@ -5,3 +5,21 @@ print(5 in nums)
 print(10 not in nums)
 
 #concatenation can't be done in sets
+#Can't repeat sets
+
+weekdays=('Mon','Tue','Wed','Thu','Fri','Sat','Sun') 
+print(set(weekdays)) #in the output order of weekdays gets changing every time that proves that sets are non-sequntial
+
+# sets are mutable
+#add
+nums.add(5)
+print(nums)
+
+#remove
+nums.remove(5) #It gives error when element is not present in the set
+print(nums)
+
+#discard
+nums.discard(0)
+nums.discard(10) #It not give error when element is not present in the set
+print(nums)
