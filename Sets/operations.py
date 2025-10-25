1c7849218cc5f81759eb8a0ad9bd58bcad20bd3c
YagnaRaval28 <yagnaraval4@gmail.com>
2025-10-25 17:09:44 +0530

    Created sets folder and added set1.py

---
+++ Sets/operations.py
@@ -0,0 +1,7 @@
+#Membership operator
+nums={0,1,2,38,9,4}
+print(0 in nums)
+print(5 in nums)
+print(10 not in nums)
+
+#concatenation can't be done in sets
---
+++ Sets/set1.py
@@ -0,0 +1,10 @@
+# sets are non-sequential collection of items
+# comma separated elements are enclosed within {}
+# sets does not allow duplicate elements
+
+set={1,"Python",1.1,1,1,1,1,1,1,1,1}
+print(set)
+print(type(set))
+
+print(len(set))
+
