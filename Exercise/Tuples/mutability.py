e91471a8425b205531f7708ceda436fbe487fd5a
YagnaRaval28 <yagnaraval4@gmail.com>
2025-10-25 16:46:18 +0530

    Added mutability.py

---
+++ Exercise/Tuples/mutability.py
@@ -0,0 +1,8 @@
+# Lists are mutable
+# Tuples and Strings are immutable
+# Tuple cannot be changed
+s1="Python is fun"
+s1.replace("Python","Java")
+print(s1)
+s2=s1.replace("Python","Java")
+print(s2)
