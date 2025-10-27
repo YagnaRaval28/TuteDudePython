32da1e5e4a9e8b0d17163f694129c87a4ae1c120
YagnaRaval28 <yagnaraval4@gmail.com>
2025-10-27 15:46:14 +0530

    For loop basic

---
+++ Exercise/Loops/for_loop.py
@@ -0,0 +1,29 @@
+# Syntax
+ 
+"""
+for var in squence:
+    statement1
+    statement2
+    ...
+    ...
+    ...
+    statementN
+"""
+
+# For loop on lists
+"""
+percents=[90,85,87,68,86]
+for p in percents:
+    print(p)
+"""
+# For loop on string
+"""
+str="Hello World"
+for char in str:
+    print(char)        
+"""
+
+# For loop on dict
+employee={'Name':'John','empID':1001,'Department':"HR"}
+for key in employee:
+    print(f"{key} -> {employee[key]}")
