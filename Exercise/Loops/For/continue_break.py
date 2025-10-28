c3e4d0d26d0d9ab596ea104a443e9e25d878ad65
YagnaRaval28 <yagnaraval4@gmail.com>
2025-10-28 07:29:54 +0530

    Continue and break in Python

---
+++ Exercise/Loops/For/continue_break.py
@@ -0,0 +1,14 @@
+"""
+for num in range(10):
+    if num%3==0:
+        continue
+    print(num)
+    """
+# Output=1,2,4,5,7,8
+
+
+for num in range(1,10):
+    if num%3==0:
+        break
+    print(num)
+# Outpt=1,2    
