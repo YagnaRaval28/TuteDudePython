33ef0152eedf5b342629233aa0d3001c5a790525
YagnaRaval28 <yagnaraval4@gmail.com>
2025-11-02 14:06:12 +0530

    append data into file

---
+++ Exercise/File Handling/append.py
@@ -0,0 +1,8 @@
+# if file doesn't exists 'a' mode creates file
+fh = open("example.txt", "at")
+
+fh.write("\nThis content has been written through 'a' mode\n")
+fh.write("'a' mode is used to add new content at the end of the file\n")
+fh.write("Good bye!\n")
+
+fh.close()
